2e023572e73c9cf8babab12bb0e06f3b4c7ffece
David <powdavyd@gmail.com>
2021-11-12 13:35:29 +0100

    LETS FUCKING GOOOOO

---
+++ TresEnRaya/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace TresEnRaya
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn0_0 = new System.Windows.Forms.Button();
+            this.btn1_0 = new System.Windows.Forms.Button();
+            this.btn2_0 = new System.Windows.Forms.Button();
+            this.btn0_1 = new System.Windows.Forms.Button();
+            this.btn1_1 = new System.Windows.Forms.Button();
+            this.btn2_1 = new System.Windows.Forms.Button();
+            this.btn0_2 = new System.Windows.Forms.Button();
+            this.btn1_2 = new System.Windows.Forms.Button();
+            this.btn2_2 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblVictoria = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn0_0
+            // 
+            this.btn0_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0_0.Location = new System.Drawing.Point(93, 51);
+            this.btn0_0.Name = "btn0_0";
+            this.btn0_0.Size = new System.Drawing.Size(90, 90);
+            this.btn0_0.TabIndex = 0;
+            this.btn0_0.UseVisualStyleBackColor = true;
+            this.btn0_0.Click += new System.EventHandler(this.btn0_0_Click);
+            // 
+            // btn1_0
+            // 
+            this.btn1_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1_0.Location = new System.Drawing.Point(182, 51);
+            this.btn1_0.Name = "btn1_0";
+            this.btn1_0.Size = new System.Drawing.Size(90, 90);
+            this.btn1_0.TabIndex = 1;
+            this.btn1_0.UseVisualStyleBackColor = true;
+            this.btn1_0.Click += new System.EventHandler(this.btn1_0_Click);
+            // 
+            // btn2_0
+            // 
+            this.btn2_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.btn2_0.Location = new System.Drawing.Point(271, 51);
+            this.btn2_0.Name = "btn2_0";
+            this.btn2_0.Size = new System.Drawing.Size(90, 90);
+            this.btn2_0.TabIndex = 2;
+            this.btn2_0.UseVisualStyleBackColor = true;
+            this.btn2_0.Click += new System.EventHandler(this.btn2_0_Click);
+            // 
+            // btn0_1
+            // 
+            this.btn0_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.btn0_1.Location = new System.Drawing.Point(93, 140);
+            this.btn0_1.Name = "btn0_1";
+            this.btn0_1.Size = new System.Drawing.Size(90, 90);
+            this.btn0_1.TabIndex = 3;
+            this.btn0_1.UseVisualStyleBackColor = true;
+            this.btn0_1.Click += new System.EventHandler(this.btn0_1_Click);
+            // 
+            // btn1_1
+            // 
+            this.btn1_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.btn1_1.Location = new System.Drawing.Point(182, 140);
+            this.btn1_1.Name = "btn1_1";
+            this.btn1_1.Size = new System.Drawing.Size(90, 90);
+            this.btn1_1.TabIndex = 4;
+            this.btn1_1.UseVisualStyleBackColor = true;
+            this.btn1_1.Click += new System.EventHandler(this.btn1_1_Click);
+            // 
+            // btn2_1
+            // 
+            this.btn2_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.btn2_1.Location = new System.Drawing.Point(271, 140);
+            this.btn2_1.Name = "btn2_1";
+            this.btn2_1.Size = new System.Drawing.Size(90, 90);
+            this.btn2_1.TabIndex = 5;
+            this.btn2_1.UseVisualStyleBackColor = true;
+            this.btn2_1.Click += new System.EventHandler(this.btn2_1_Click);
+            // 
+            // btn0_2
+            // 
+            this.btn0_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.btn0_2.Location = new System.Drawing.Point(93, 229);
+            this.btn0_2.Name = "btn0_2";
+            this.btn0_2.Size = new System.Drawing.Size(90, 90);
+            this.btn0_2.TabIndex = 6;
+            this.btn0_2.UseVisualStyleBackColor = true;
+            this.btn0_2.Click += new System.EventHandler(this.btn0_2_Click);
+            // 
+            // btn1_2
+            // 
+            this.btn1_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.btn1_2.Location = new System.Drawing.Point(182, 229);
+            this.btn1_2.Name = "btn1_2";
+            this.btn1_2.Size = new System.Drawing.Size(90, 90);
+            this.btn1_2.TabIndex = 7;
+            this.btn1_2.UseVisualStyleBackColor = true;
+            this.btn1_2.Click += new System.EventHandler(this.btn1_2_Click);
+            // 
+            // btn2_2
+            // 
+            this.btn2_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.btn2_2.Location = new System.Drawing.Point(271, 229);
+            this.btn2_2.Name = "btn2_2";
+            this.btn2_2.Size = new System.Drawing.Size(90, 90);
+            this.btn2_2.TabIndex = 8;
+            this.btn2_2.UseVisualStyleBackColor = true;
+            this.btn2_2.Click += new System.EventHandler(this.btn2_2_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(162, 353);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(128, 51);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "RESET";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblVictoria
+            // 
+            this.lblVictoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.lblVictoria.Location = new System.Drawing.Point(62, 436);
+            this.lblVictoria.Name = "lblVictoria";
+            this.lblVictoria.Size = new System.Drawing.Size(331, 52);
+            this.lblVictoria.TabIndex = 10;
+            this.lblVictoria.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 518);
+            this.Controls.Add(this.lblVictoria);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btn2_2);
+            this.Controls.Add(this.btn1_2);
+            this.Controls.Add(this.btn0_2);
+            this.Controls.Add(this.btn2_1);
+            this.Controls.Add(this.btn1_1);
+            this.Controls.Add(this.btn0_1);
+            this.Controls.Add(this.btn2_0);
+            this.Controls.Add(this.btn1_0);
+            this.Controls.Add(this.btn0_0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn0_0;
+        private System.Windows.Forms.Button btn1_0;
+        private System.Windows.Forms.Button btn2_0;
+        private System.Windows.Forms.Button btn0_1;
+        private System.Windows.Forms.Button btn1_1;
+        private System.Windows.Forms.Button btn2_1;
+        private System.Windows.Forms.Button btn0_2;
+        private System.Windows.Forms.Button btn1_2;
+        private System.Windows.Forms.Button btn2_2;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblVictoria;
+    }
+}
+
---
+++ TresEnRaya/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TresEnRaya.Properties;
+
+namespace TresEnRaya
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private static List<String> x = new List<string>();
+        private static List<String> o = new List<string>();
+
+        private static int contTurnos = 1;
+
+        
+
+        private void btn0_0_Click(object sender, EventArgs e)
+        {
+            turno("0.0");
+
+            evaluarTurno();
+        }
+
+        private void btn1_0_Click(object sender, EventArgs e)
+        {
+            turno("1.0");
+            evaluarTurno();
+
+
+        }
+
+        private void btn2_0_Click(object sender, EventArgs e)
+        {
+            turno("2.0");
+            evaluarTurno();
+
+        }
+
+        private void btn0_1_Click(object sender, EventArgs e)
+        {
+            turno("0.1");
+            evaluarTurno();
+
+        }
+
+        private void btn1_1_Click(object sender, EventArgs e)
+        {
+            turno("1.1");
+            evaluarTurno();
+
+        }
+
+        private void btn2_1_Click(object sender, EventArgs e)
+        {
+            turno("2.1");
+            evaluarTurno();
+
+        }
+
+        private void btn0_2_Click(object sender, EventArgs e)
+        {
+            turno("0.2");
+            evaluarTurno();
+
+        }
+
+        private void btn1_2_Click(object sender, EventArgs e)
+        {
+            turno("1.2");
+            evaluarTurno();
+
+        }
+
+        private void btn2_2_Click(object sender, EventArgs e)
+        {
+            turno("2.2");
+            evaluarTurno();
+
+        }
+
+        public void turno(String boton)
+        {
+            String turno = "";
+            if (contTurnos % 2 == 0)
+            {
+                turno = "o";
+            } else
+            {
+                turno = "x";
+            }
+
+            contTurnos++;
+
+            switch (boton)
+            {
+                case "0.0":
+                    if (turno.Equals("o"))
+                    {
+                        btn0_0.Text = "o";
+                        o.Add(boton);
+                    } else
+                    {
+                        btn0_0.Text = "x";
+                        x.Add(boton);
+                    }
+                    btn0_0.Enabled = false;
+                break;
+
+                case "0.1":
+                    if (turno.Equals("o"))
+                    {
+                        btn0_1.Text = "o";
+                        o.Add(boton);
+
+                    }
+                    else
+                    {
+                        btn0_1.Text = "x";
+                        x.Add(boton);
+
+                    }
+                    btn0_1.Enabled = false;
+
+                break;
+
+                case "0.2":
+                    if (turno.Equals("o"))
+                    {
+                        btn0_2.Text = "o";
+                        o.Add(boton);
+
+                    }
+                    else
+                    {
+                        btn0_2.Text = "x";
+                        x.Add(boton);
+
+                    }
+                    btn0_2.Enabled = false;
+
+                break;
+
+                case "1.0":
+                    if (turno.Equals("o"))
+                    {
+                        btn1_0.Text = "o";
+                        o.Add(boton);
+
+                    }
+                    else
+                    {
+                        btn1_0.Text = "x";
+                        x.Add(boton);
+
+                    }
+                    btn1_0.Enabled = false;
+
+                break;
+
+                case "1.1":
+                    if (turno.Equals("o"))
+                    {
+                        btn1_1.Text = "o";
+                        o.Add(boton);
+
+                    }
+                    else
+                    {
+                        btn1_1.Text = "x";
+                        x.Add(boton);
+
+                    }
+                    btn1_1.Enabled = false;
+
+                break;
+
+                case "1.2":
+                    if (turno.Equals("o"))
+                    {
+                        btn1_2.Text = "o";
+                        o.Add(boton);
+
+                    }
+                    else
+                    {
+                        btn1_2.Text = "x";
+                        x.Add(boton);
+
+                    }
+                    btn1_2.Enabled = false;
+
+                break;
+
+                case "2.0":
+                    if (turno.Equals("o"))
+                    {
+                        btn2_0.Text = "o";
+                        o.Add(boton);
+
+                    }
+                    else
+                    {
+                        btn2_0.Text = "x";
+                        x.Add(boton);
+
+                    }
+                    btn2_0.Enabled = false;
+
+                break;
+
+                case "2.1":
+                    if (turno.Equals("o"))
+                    {
+                        btn2_1.Text = "o";
+                        o.Add(boton);
+
+                    }
+                    else
+                    {
+                        btn2_1.Text = "x";
+                        x.Add(boton);
+
+                    }
+                    btn2_1.Enabled = false;
+
+                break;
+
+                case "2.2":
+                    if (turno.Equals("o"))
+                    {
+                        btn2_2.Text = "o";
+                        o.Add(boton);
+
+                    }
+                    else
+                    {
+                        btn2_2.Text = "x";
+                        x.Add(boton);
+
+                    }
+                    btn2_2.Enabled = false;
+
+                break;
+
+
+            }
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            contTurnos = 1;
+            btn0_0.Enabled = true;
+            btn1_1.Enabled = true;
+            btn2_2.Enabled = true;
+            btn0_1.Enabled = true;
+            btn0_2.Enabled = true;
+            btn1_0.Enabled = true;
+            btn2_0.Enabled = true;
+            btn2_1.Enabled = true;
+            btn1_2.Enabled = true;
+
+            btn0_0.Text = "";
+            btn1_1.Text = "";
+            btn2_2.Text = "";
+            btn0_1.Text = "";
+            btn0_2.Text = "";
+            btn1_0.Text = "";
+            btn2_0.Text = "";
+            btn2_1.Text = "";
+            btn1_2.Text = "";
+
+            lblVictoria.Text = "";
+
+           x.Clear();
+           o.Clear();
+        }
+
+        private void evaluarTurno()
+        {
+            String victoriaO = "GANA O";
+            String victoriaX = "GANA X";
+
+            Console.WriteLine("O:");
+
+            for (int i = 0; i < o.Count(); i++) {
+                Console.Write(o[i] + ", ");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("X:");
+
+            for (int i = 0; i < x.Count(); i++)
+            {
+                Console.Write(x[i] + ", ");
+
+            }
+
+            if ((x.Contains("0.0") && x.Contains("1.1") && x.Contains("2.2")) || (x.Contains("2.0") && x.Contains("1.1") && x.Contains("0.2")) ||
+                (x.Contains("0.0") && x.Contains("0.1") && x.Contains("0.2")) || (x.Contains("1.0") && x.Contains("1.1") && x.Contains("1.2")) || (x.Contains("2.0") && x.Contains("2.1") && x.Contains("2.2")) ||
+                (x.Contains("0.0") && x.Contains("1.0") && x.Contains("2.0")) || (x.Contains("0.1") && x.Contains("1.1") && x.Contains("2.1")) || (x.Contains("0.2") && x.Contains("1.2") && x.Contains("2.2")))
+                {
+                    declararVictoria();
+                    lblVictoria.Text = victoriaX;
+                }
+                else if ((o.Contains("0.0") && o.Contains("1.1") && o.Contains("2.2")) || (o.Contains("2.0") && o.Contains("1.1") && o.Contains("0.2")) ||
+                  (o.Contains("0.0") && o.Contains("0.1") && o.Contains("0.2")) || (o.Contains("1.0") && o.Contains("1.1") && o.Contains("1.2")) || (o.Contains("2.0") && o.Contains("2.1") && o.Contains("2.2")) ||
+                  (o.Contains("0.0") && o.Contains("1.0") && o.Contains("2.0")) || (o.Contains("0.1") && o.Contains("1.1") && o.Contains("2.1")) || (o.Contains("0.2") && o.Contains("1.2") && o.Contains("2.2")))
+                {
+                    declararVictoria();
+                    lblVictoria.Text = victoriaO;
+                } else if (contTurnos == 10)
+                {
+                    declararVictoria();
+                    lblVictoria.Text = "EMPATE";
+                }
+             
+
+            
+
+
+        }
+
+        private void declararVictoria()
+        {
+            btn0_0.Enabled = false;
+            btn1_1.Enabled = false;
+            btn2_2.Enabled = false;
+            btn0_1.Enabled = false;
+            btn0_2.Enabled = false;
+            btn1_0.Enabled = false;
+            btn2_0.Enabled = false;
+            btn2_1.Enabled = false;
+            btn1_2.Enabled = false;
+        }
+    }
+}
